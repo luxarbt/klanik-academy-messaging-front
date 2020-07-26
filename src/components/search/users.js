a20fb2ba078c6f74c915a6ac6479cdd68d991161
Ribot Alexis <alexisribot10@gmail.com>
2020-07-26 22:44:29 +0200

    send chat request, todo : send notifications

--- src/components/search/users.js
+++ src/components/search/users.js
@@ -1,18 +1,45 @@
+/* eslint-disable import/prefer-default-export */
 import React, { useContext } from "react"
+import Axios from "axios"
 import UserContext from "../../context/UserContext"
 
 export const Users = ({users}) => {
   const { userData } = useContext(UserContext)
+
+  const currentUser = undefined !== userData.user ? userData.user : ""
+
+  const sendChatRequest = async (e) => {
+    try {
+      const newChatRequest = {
+        userRequesting: currentUser,
+        userRequested: JSON.parse(e.target.dataset.user),
+        status: "pending",
+      }
+      await Axios.post("http://localhost:9000/chat/newrequest", newChatRequest)
+
+      // TODO : Send notification
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <div>
       <h3>Users : </h3>
-      {users.map((user, i) => (
-        <div key={i}>
+      <ul>
+        {users.map((user) => (
           <li>
             {user.name} {user.surname}
+            <input
+              type="button"
+              className="btn btn-primary btn-block"
+              value="Request chat"
+              data-user={JSON.stringify(user)}
+              onClick={sendChatRequest}
+            />
           </li>
-        </div>
-      ))}
+        ))}
+      </ul>
     </div>
   )
 }
