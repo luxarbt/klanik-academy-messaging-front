cbfaf8d5d1a835c18844b161df20cf0055ff3324
Ribot Alexis <alexisribot10@gmail.com>
2020-07-31 21:43:40 +0200

    get request

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,32 @@
-import React, { useContext } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import Axios from "axios"
 import UserContext from "../../context/UserContext"
 import Search from "../search/search"
 
 export default function Home() {
   const { userData } = useContext(UserContext)
+
+  const [requests, setRequests] = useState([])
+  useEffect(() => {
+    if (undefined !== userData.data) {
+      const getRequests = async () => {
+        const result = await Axios.get(
+          "http://localhost:9000/chat/requestget",
+          {
+            params: {
+              userRequested: userData.user._id,
+            },
+          }
+        ).then((response) => setRequests(response.data))
+      }
+      getRequests()
+    }
+  })
+
+  const toto = requests.map((request) => {
+    return request
+  })
 
   return (
     <div className="page">
