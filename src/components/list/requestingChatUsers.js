9dfa9d711c26a0a8afad6baa752bd109fa7441ed
Ribot Alexis <alexisribot10@gmail.com>
2020-08-05 01:59:30 +0200

    update status request

--- src/components/list/requestingChatUsers.js
+++ src/components/list/requestingChatUsers.js
@@ -6,11 +6,13 @@ export default function RequestingChatUsers() {
   const { userData } = useContext(UserContext)
 
   const [users, setUsers] = useState([])
+  const [requestChat, setRequestChat] = useState()
 
   const apiCall = async () => {
     Axios.get("http://localhost:9000/chat/requestget", {
       params: { userRequested: userData },
     }).then((response) => {
+      setRequestChat(response.data)
       response.data.map(async (request) => {
         try {
           const usersRequesting = await Axios.get(
@@ -32,6 +34,40 @@ export default function RequestingChatUsers() {
     apiCall()
   }, [userData])
 
+  const sendChatUpdate = async () => {
+    try {
+      const acceptChatRequest = {
+        chatRequestId: requestChat[0]._id,
+        status: "accepted",
+      }
+      await Axios.put(
+        "http://localhost:9000/chat/updaterequest",
+        acceptChatRequest
+      )
+
+      // TODO : Send notification
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const sendChatUpdateDecline = async () => {
+    try {
+      const declineChatRequest = {
+        chatRequestId: requestChat[0]._id,
+        status: "declined",
+      }
+      await Axios.put(
+        "http://localhost:9000/chat/updaterequest",
+        declineChatRequest
+      )
+
+      // TODO : Send notification
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <div>
       Requêtes de chat reçues :
@@ -43,11 +79,13 @@ export default function RequestingChatUsers() {
               type="button"
               className="btn btn-primary btn-block"
               value="Accept"
+              onClick={sendChatUpdate}
             />
             <input
               type="button"
               className="btn btn-primary btn-block"
               value="Decline"
+              onClick={sendChatUpdateDecline}
             />
           </>
         ))}
