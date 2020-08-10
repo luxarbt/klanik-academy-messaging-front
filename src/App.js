c1581c7981e34be00b9e612b68c69a673492cc47
Ribot Alexis <alexisribot10@gmail.com>
2020-08-10 21:59:07 +0200

    fix call route

--- src/App.js
+++ src/App.js
@@ -1,47 +1,47 @@
-import React, { useState, useEffect } from "react"
-import { BrowserRouter, Switch, Route } from "react-router-dom"
-import Axios from "axios"
-import Header from "./components/layout/Header"
-import Home from "./components/pages/Home"
-import Login from "./components/auth/Login"
-import Register from "./components/auth/Register"
-import UserContext from "./context/UserContext"
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Axios from "axios";
+import Header from "./components/layout/Header";
+import Home from "./components/pages/Home";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import UserContext from "./context/UserContext";
 
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import "./style.css"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./style.css";
 
 export default function App() {
   const [userData, setUserData] = useState({
     token: undefined,
     user: undefined,
-  })
+  });
 
   useEffect(() => {
     const checkLoggedIn = async () => {
-      let token = localStorage.getItem("auth-token")
+      let token = localStorage.getItem("auth-token");
       if (token === null) {
-        localStorage.setItem("auth-token", "")
-        token = ""
+        localStorage.setItem("auth-token", "");
+        token = "";
       }
       const tokenRes = await Axios.post(
         "http://localhost:9000/users/tokenIsValid",
         null,
         { headers: { "x-auth-token": token } }
-      )
+      );
       if (tokenRes.data) {
         const userRes = await Axios.get("http://localhost:9000/users/", {
           headers: { "x-auth-token": token },
-        })
+        });
 
         setUserData({
           token,
           user: userRes.data,
-        })
+        });
       }
-    }
+    };
 
-    checkLoggedIn()
-  }, [])
+    checkLoggedIn();
+  }, []);
 
   return (
     <>
@@ -62,5 +62,5 @@ export default function App() {
         </div>
       </BrowserRouter>
     </>
-  )
+  );
 }
